8e86ec7ca668109afe4a39e5905147eeeb7070ee
Lucas Rocha <lucasbezerrar@gmail.com>
2024-01-28 00:38:11 -0300

    Add classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./Header.css"
+import { useState } from "react"
 
 export const Header = () => {
+  const [activeClass, setActiveClass] = useState(null)
+
+  const { pathname } = useLocation()
+
   return(
     <>
       <header className="header-content">
@@ -10,11 +15,20 @@ export const Header = () => {
         </figure>
         <nav className="menu">
           <ul>
-            <Link to='/'>Home</Link>
-            <Link to='/jogadores'>Jogadores</Link>
-            <Link to='/sobre'>Sobre</Link>
+            <Link to='/' className={pathname === '/' ? 'active' : ''}>
+              Home
+            </Link>
+
+            <Link to='/jogadores' className={pathname === '/jogadores' ? 'active' : ''}>
+              Jogadores
+            </Link>
+
+            <Link to='/sobre' className={pathname === '/sobre' ? 'active' : ''}>
+              Sobre
+            </Link>
           </ul>
         </nav>
+        <p>ao clicar receber a classe active</p>
       </header>
     </>
   )
